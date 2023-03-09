879cda7ba8d9e3af238f7888f545493840f89114
Omer Meidan <Ome12001@gmail.com>
2023-03-09 16:10:25 +0200

    bla

--- src/assets/screens/MyGames.jsx
+++ src/assets/screens/MyGames.jsx
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState, useContext } from "react";
+import { AuthContext } from '../../context/AuthContext';
 import { Text, View } from 'react-native'
-
+import axios from 'axios'
 const MyGames = () => {
+    const { setToken, token,PremiumGamesArr,setPremiumGamesArr,CommunityGamesArr,setCommunityGamesArr,emailToken,userDetails,setUserDetails,myGames,setMyGames  } = useContext(AuthContext);
+    useEffect(()=>{
+        const getPlayerGames = async()=>{
+            setMyGames([])
+            try{
+                const response = await axios.post('https://tlv-hoops-server.onrender.com/gameList',{})
+                if(response.data){
+                    setMyGames(prevState => [...prevState, response.data.filter(game=>(game.participants.find(player=>player._id===userDetails._id)))]);
+                }
+            }
+            catch(error){
+                console.log(error)
+            }
+        }
+        getPlayerGames()
+    },[])
 
+    console.log('myGames')
+console.log(myGames)
     return (
         <View>
-            <Text> My Games </Text>
+            {myGames.map((array,i)=>array.map((a,j)=><Text key={j}>{a.date}</Text>))}
         </View>
     )
 
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,8 +6,11 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState('');
   const [PremiumGamesArr,setPremiumGamesArr] = useState([])
   const [CommunityGamesArr,setCommunityGamesArr] = useState([])
+  const [emailToken,setEmailToken]=useState('')
+  const [userDetails,setUserDetails] =useState({})
+  const [myGames,setMyGames] = useState([])
   return (
-    <AuthContext.Provider value={{ token, setToken,PremiumGamesArr,setPremiumGamesArr,CommunityGamesArr,setCommunityGamesArr }}>
+    <AuthContext.Provider value={{ token, setToken,PremiumGamesArr,setPremiumGamesArr,CommunityGamesArr,setCommunityGamesArr,emailToken,setEmailToken,userDetails,setUserDetails,myGames,setMyGames }}>
       {children}
     </AuthContext.Provider>
   )
